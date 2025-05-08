7ac354b067d9e183bac40d8ef6490bce268b8d08
Neil Mannion <neil.mannion1@gmail.com>
2025-05-08 16:15:02 -0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,22 @@ import './App.css'
 import Navbar from '@components/navbar/navbar.jsx';
 import Home from '@components/home/home.jsx';
 import Projects from '@components/projects/projects.jsx';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-
 function App() {
-
   return (
     <div className='page-container'>
-      <Router>
-          {/* <Navbar /> */}
-          <Routes>
-              <Route path="/" element={<Navigate to="/home"  replace/>} />
-              <Route path='/home' element={<Home />} />
-              <Route path='/projects' element={<Projects />}/>
-          </Routes>
-          {/* <Footer/> */} 
-      </Router>
-</div>
-  )
+      {/* Only one BrowserRouter needed */}
+      <BrowserRouter basename="/ReactPortfolio/"> 
+        <Routes>
+            <Route path="/" element={<Navigate to="/home" replace />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
